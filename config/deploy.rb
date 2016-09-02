aa0a3d7d1693279175cfdf3c574f12a9a7b1e110
Alex Myers <alex.myers@enspiresoftware.com>
2016-09-02 11:42:56 -0400

    update cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock '3.4.0'
 
 set :application, 'onebody'
-set :repo_url, 'git://github.com/churchio/onebody.git'
+set :repo_url, 'git://github.com/enspiresoftware/onebody.git'
 set :deploy_to, '/var/www/apps/onebody'
 
 set :linked_files, %w(config/database.yml config/email.yml config/secrets.yml config/newrelic.yml)
